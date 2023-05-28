12c86ace81d3fcc0fc8b2cf583647b1c35da3141
salkin-mada <adam@oddodd.org>
2023-05-28 13:50:23 +0200

    fix(template): use trailing return type

--- include/PicoOSC/PicoOSC.hpp
+++ include/PicoOSC/PicoOSC.hpp
@@ -38,20 +38,21 @@ static constexpr auto MAX_TIMETAG_SIZE = 8;
 // From
 // https://stackoverflow.com/questions/105252/how-do-i-convert-between-big-endian-and-little-endian-values-in-c
 template<typename T>
-T swap_endian(T u)
+auto swap_endian(T uns) -> T
 {
   static_assert(CHAR_BIT == 8, "CHAR_BIT != 8");
 
   union
   {
-    T u;
+    T uns;
     unsigned char u8[sizeof(T)];
   } source, dest;
 
-  source.u = u;
+  source.uns = uns;
 
-  for (size_t k = 0; k < sizeof(T); k++)
-    dest.u8[k] = source.u8[sizeof(T) - k - 1];
+  for (size_t k = 0; k < sizeof(T); k++) {
+      dest.u8[k] = source.u8[sizeof(T) - k - 1];
+  }
 
   return dest.u;
 }
