23917963552eae6ffa06cd82778493fc0ade41d4
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2023-05-31 21:37:07 +0200

    fix: OSCServer now reads UDP packets

--- include/PicoOSC/PicoOSC.hpp
+++ include/PicoOSC/PicoOSC.hpp
@@ -491,4 +491,135 @@ private:
   char mBuffer[MAX_MESSAGE_SIZE];
 };
 
+// A simple OSC server that can receive OSC messages using lwip
+class OSCServer
+{
+public:
+  // Constructor that takes a port number
+  OSCServer(uint16_t serverPort)
+  {
+    // Create a new OSC pcb
+    mPcb = udp_new();
+
+#if NO_SYS == 1
+    cyw43_arch_lwip_begin();
+#endif
+    err_t _bindErr = ERR_OK;
+
+    if (mPcb != nullptr
+        // (mPcb = udp_new_ip_type(IPADDR_TYPE_V4)) != NULL
+    )
+    {
+      // Set receive callback
+      setReceiveFunction();
+
+      _bindErr = udp_bind(mPcb, IP_ADDR_ANY, serverPort);
+    } else
+      _bindErr = ERR_MEM;
+#if NO_SYS == 1
+    cyw43_arch_lwip_end();
+#endif
+
+    // Bind the pcb to the port
+    // const auto result = udp_bind(mPcb, IP_ADDR_ANY, port);
+
+    if (_bindErr != ERR_OK) {
+      printf("Failed to bind server to UDP pcb! error=%d\n", _bindErr);
+    } else {
+      printf("Bound server to UDP pcb to port %d\n", serverPort);
+    }
+
+    // // Connect
+    // const auto connError = udp_connect(mPcb, IP_ADDR_ANY, serverPort);
+    //
+    // if (connError != ERR_OK) {
+    //   printf("Failed to connect UDP pcb! error=%d\n", connError);
+    // } else {
+    //   printf("Connected UDP pcb to port %d\n", serverPort);
+    // };
+  }
+
+  // Destructor
+  ~OSCServer()
+  {
+    // Close the pcb
+    udp_remove(mPcb);
+  }
+
+  // Receive packet
+  // Returns a an OSC message if a packet was received
+  // and a nullptr if no packet was received
+  void setReceiveFunction()
+  {
+
+
+    // Attempt to receive the packet
+    udp_recv(
+        mPcb,
+        [](
+            void* arg, udp_pcb* pcb, pbuf* p, const ip_addr_t* addr, u16_t port)
+        {
+          // Get ip of incoming packet
+          const auto ip = ipaddr_ntoa(addr);
+
+          // Get the length of the packet
+          const auto len = p->len;
+
+          if (p != nullptr) {
+            // Get the payload of the packet
+            const auto payload = reinterpret_cast<const char*>(p->payload);
+
+            // Print
+            printf(
+                "Received packet from %s:%d with length %d\n", ip, port, len);
+
+            //------------------------------------------------------------------//
+            //                        Parse OSC message //
+            //------------------------------------------------------------------//
+
+            // Parse as OSC message
+            OSCMessage message;
+            message.parse(payload, len);
+            message.print();
+            const auto typeTag = message.getTypeTag();
+
+            if (typeTag != nullptr) {
+              printf("Type tag: %s\n", typeTag);
+
+			  // print tag position
+			  const auto typeTagPosition = message.findTypeTagPosition();
+			  printf("Type tag position: %d\n", typeTagPosition);
+
+              // print arguments
+              const auto arguments = message.getArguments();
+
+			  // print argument position
+			  const auto argumentsPosition = message.findArgumentsPosition();
+			  printf("Arguments position: %d\n", argumentsPosition);
+
+              if (arguments != nullptr) {
+                // Print arguments
+                printf("Arguments: ");
+                for (int i = 0; i < std::strlen(arguments); i++) {
+                  printf("%c", arguments[i]);
+                }
+                printf("\n");
+
+                // Print float
+                const auto value = message.getInt();
+                printf("Value: %d\n", value);
+              }
+            }
+
+            // Free pbuffer
+            pbuf_free(p);
+          }
+        },
+        NULL);
+  }
+
+private:
+  udp_pcb* mPcb;
+};
+
 }  // namespace picoosc
